ce548248900639ca3267ff51a20c02ae94966778
nathanwashere_ <normuradov1305@gmail.com>
2025-12-07 12:53:01 +0200

    Added register validation

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,10 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
+using Tasman.Data;
 using Tasman.Models;
 
 namespace Tasman.Controllers
 {
     public class UserController : Controller
     {
+        // For database access
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         // GET: User
         public ActionResult Index()
         {
@@ -13,8 +21,21 @@ namespace Tasman.Controllers
         [HttpPost]
         public ActionResult Create(User _user)
         {
+            if (ModelState.IsValid)
+            {
+                if(_context.Users.Any(u => u.Email == _user.Email))
+                {
+                    ModelState.AddModelError("Email", "Email already exists");
+                    return View("/Views/Register/Register.cshtml", _user);
+                }
+                _context.Users.Add(_user);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index","Travel");
+            }
             
-            return RedirectToAction("Index","Travel");
+            return View("/Views/Register/Register.cshtml", _user);
+            
         }
     }
 }
